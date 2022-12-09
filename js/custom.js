3e4968fbc0e23e0f8cfcb595b054ca1d4cb170f4
swagata-codearts <swagata.codearts@gmail.com>
2022-12-09 11:19:37 +0530

    Header, banner, welcome & footer

--- js/custom.js
+++ js/custom.js
@@ -16,14 +16,35 @@ jQuery('#custom-owl').owlCarousel({
     }
 });
 
+function getDocHeight() {
+    var D = document;
+    return Math.max(
+        D.body.scrollHeight, D.documentElement.scrollHeight,
+        D.body.offsetHeight, D.documentElement.offsetHeight,
+        D.body.clientHeight, D.documentElement.clientHeight
+    );
+}
 jQuery(document).ready(function(){
     jQuery(window).scroll(function() {
        if(jQuery(this).scrollTop() > 200 ) {
        jQuery(".custom-header").addClass("fixed-top");
        jQuery(".header-top").slideUp();
+       jQuery(".banner_socials").addClass("slide-left");
       } else {
        jQuery(".custom-header").removeClass("fixed-top");
        jQuery(".header-top").slideDown();
+       jQuery(".banner_socials").removeClass("slide-left");
       }
+    });
+    jQuery(window).scroll(function() {
+       if(jQuery(this).scrollTop() > 400 ) {
+        jQuery(".banner_socials").removeClass("slide-left").addClass("sticky-left");
+       } else if(Math.round(jQuery(window).scrollTop()) + jQuery(window).innerHeight() == jQuery(document).height()) {
+        loadPagination();
+        alert("2210")
+        jQuery(".banner_socials").removeClass("sticky-left").addClass("slide-left");
+        
+       }
+      
     });
 });
